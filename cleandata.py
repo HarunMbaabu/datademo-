d1612cc25fe02dfe0f00eb6bd02b2114eb31e335
harunmwenda <hmwenda@mfsafrica.com>
2022-07-14 18:53:14 +0300

    add more content

--- cleandata.py
+++ cleandata.py
@@ -3,4 +3,14 @@ import pandas as pd
 import numpy as np 
 
 
-data = pd.read_csv("Iris.csv")
+data = pd.read_csv("Iris.csv") 
+
+data.describe() 
+
+data.head() 
+
+data.tail()
+
+data.shape()
+
+data.info
